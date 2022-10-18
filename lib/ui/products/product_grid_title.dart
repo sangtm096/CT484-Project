a3514004ac4f56eb3d9c34801168319145ef7240
Sang <sangb1910440@student.ctu.edu.vn>
2022-10-18 08:50:48 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/ui/products/product_grid_title.dart
+++ lib/ui/products/product_grid_title.dart
@@ -1,63 +1,69 @@
 import 'package:flutter/material.dart';
-import '../../models/product.dart';
-import 'product_detail_screen.dart';
 
+import '../../models/product.dart';
+
+import 'product_detail_screen.dart';
 
 class ProductGridTile extends StatelessWidget {
   const ProductGridTile(
     this.product, {
       super.key,
-    });
+  });
 
-    final Product product;
+  final Product product;
 
-    @override
-    Widget build(BuildContext context) {
-      return ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          footer: buildGridFooterBar(context),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(
-                ProductDetailScreen.routeName,
-                arguments: product.id,
-              );
-            },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: buildGridFooterBar(context),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child:  Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildGridFooterBar(BuildContext context) {
+    return GridTileBar(
+      backgroundColor: Colors.black87,
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable,
+        builder: (ctx, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
             ),
-          ),
-        ),       
-      );
-    }
-
-    Widget buildGridFooterBar(BuildContext context) {
-      return GridTileBar(
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          icon: Icon(
-            product.isFavorite ? Icons.favorite : Icons.favorite_border,
-          ),
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {
-            print('Toggle a favorite product');
-          },
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              product.isFavorite = !isFavorite;
+            },
+          );
+        },
+      ),
+      title: Text(
+        product.title,
+        textAlign: TextAlign.center,
+      ),
+      trailing: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
         ),
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
-        ),
-        trailing: IconButton(
-          icon: const Icon(
-            Icons.shopping_cart,
-          ),
-          onPressed: () {
-            print('Add item to cart');
-          },
-          color: Theme.of(context).colorScheme.secondary,
-        ),
-      );
-    }
+        onPressed: () {
+          print('Add item to cart');
+        },
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+    );
+  }
 }
