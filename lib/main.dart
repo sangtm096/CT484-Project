29acff93acf026926e36986a3bcdd99a9b60dad7
Sang <sangb1910440@student.ctu.edu.vn>
2022-10-18 22:23:49 +0700

    Quan ly trang thai voi provider - OrdersManager

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => CartManager(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => OrdersManager(), 
+        ),
       ],
       child: MaterialApp(
         title: 'Hello',
