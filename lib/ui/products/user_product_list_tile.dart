32c399fa9daee2b84f3c5458e70c05053bc6721a
Sang <sangb1910440@student.ctu.edu.vn>
2022-10-19 20:58:30 +0700

    Xay dung trang hieu chinh san pham

--- lib/ui/products/user_product_list_tile.dart
+++ lib/ui/products/user_product_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/edit_product_screen.dart';
 import 'package:myshop/ui/products/products_manager.dart';
 
 import '../../models/product.dart';
@@ -55,7 +56,10 @@ class UserProductListTile extends StatelessWidget {
       return IconButton(
         icon: const Icon(Icons.edit),
         onPressed: () {
-          print('Go to edit product screen');
+          Navigator.of(context).pushNamed(
+            EditProductScreen.routeName,
+            arguments: product.id,
+          );
         },
         color: Theme.of(context).primaryColor,
       );
